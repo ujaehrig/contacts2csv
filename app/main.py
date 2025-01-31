619957e297e56d337d956bad501e28d43e54e84d
Ulf Jährig <ulf.jaehrig@tipico.com>
2025-01-31 09:20:32 +0100

    Commit missing app

---
+++ app/main.py
@@ -0,0 +1,37 @@
+import logging
+from fastapi import FastAPI, Request, UploadFile, File
+from fastapi.responses import HTMLResponse, Response
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from library import csv2ical
+from mangum import Mangum
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+app = FastAPI()
+handler = Mangum(app)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def upload_form(request: Request):
+    return templates.TemplateResponse("upload.html", {"request": request})
+
+
+@app.post("/process/")
+async def process_file(file: UploadFile = File(...)):
+    contacts = csv2ical.process_csv(file.file)
+    return Response(
+        content=csv2ical.generate_ical(contacts),
+        media_type="text/calendar",
+        headers={"Content-Disposition": "attachment; filename=birthdays.ical"}
+    )
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
